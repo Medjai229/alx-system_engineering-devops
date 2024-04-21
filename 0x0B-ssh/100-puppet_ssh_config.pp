690f5c479f445239e200dc2f137e47db39c8213b
Malik <malikhussein229@gmail.com>
2024-04-21 08:16:23 -0700

    Task#100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,9 @@
 # Setting up my client config file
 
-file_line { 'ignore passwd auth':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
-  replace => true,
-}
-
-file_line { 'set identity file':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentifyFile ~/.ssh/school',
-  replace => true,
+file { '/etc/ssh/ssh_config':
+  ensure =>file,
+  content=> "Host *
+  	IdentityFile ~/.ssh/school
+  	PasswordAuthentication no
+  ",
 }
