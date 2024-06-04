7f4cfb2b4ec765806028bdfa38d97e5c45f0458a
Malik <malikhussein229@gmail.com>
2024-06-04 23:35:57 +0300

    Task#100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,49 +5,43 @@ and prints a sorted count of given keywords
 """
 import requests
 
-def count_words(subreddit, word_list, instances=None, after=None, count=0):
+
+def count_words(subreddit, word_list, after=None, counts={}):
     """
     Recursively queries the Reddit API, parses the title of all hot articles,
     and prints a sorted count of given keywords.
     """
-    if instances is None:
-        instances = {}
+    if not word_list or word_list == [] or not subreddit:
+        return
 
     session = requests.Session()
     session.timeout = 5
 
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     header = {"User-Agent": "Reddit word search (https://github.com/Medjai229)"}
-    params = {"after": after, "count": count, "limit": 100}
+    params = {"limit": 100}
+    if after:
+        params["after"] = after
+
     response = session.get(url, headers=header, params=params, allow_redirects=False)
-
-    try:
-        data = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
+    
+    if response.status_code != 200:
         return
-
+    
+    data = response.json()
     after = data["data"]["after"]
-    count += data["data"]["dist"]
     hot_posts = data["data"]["children"]
-    for post in hot_posts:
-        title = post["data"]["title"].lower().split()
-        for word in set(word_list):  # Converting word_list to a set to eliminate duplicates
-            word_lower = word.lower()
-            num_of_times = len([t for t in title if t == word_lower])
-            if word_lower not in instances:
-                instances[word_lower] = num_of_times
-            else:
-                instances[word_lower] += num_of_times
 
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[-1], kv[0]))
-        for key, value in instances:
-            print(f"{key.lower()}: {value}")
+    for post in hot_posts:
+        title = post["data"]["title"].lower()
+        for word in word_list:
+            if word.lower() in title:
+                counts[word] = counts.get(word, 0) + title.count(word.lower())
+
+    if after:
+        count_words(subreddit, word_list, after, counts)
     else:
-        count_words(subreddit, word_list, instances, after, count)
+        sorted_counts = sorted(counts.items(),
+                               key=lambda kv: (-kv[1], kv[0].lower()))
+        for word, count in sorted_counts:
+            print(f"{word.lower()}: {count}")
