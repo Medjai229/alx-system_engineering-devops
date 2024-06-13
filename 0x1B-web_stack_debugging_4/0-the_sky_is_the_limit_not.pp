110640ff49d903ee2dda70aeac7a50159cb19fdd
Malik <malikhussein229@gmail.com>
2024-06-13 22:49:34 +0300

    Task#0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,6 +4,7 @@ exec { 'increase_limit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
+
 exec { 'restart_nginx':
   command => 'nginx restart',
   path    => '/etc/init.d/'
