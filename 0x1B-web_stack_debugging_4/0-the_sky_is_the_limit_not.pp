96e85786847a689fd1e6855819903ce4a6b21704
Malik <malikhussein229@gmail.com>
2024-06-13 22:27:14 +0300

    Task#0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# Increase the limit of nginx requests
+
+exec { 'increase_limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+exec { 'restart_nginx':
+  command => 'nginx restart',
+}
