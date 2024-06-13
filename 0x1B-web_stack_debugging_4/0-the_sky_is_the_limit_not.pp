a97e8bdaad28983e24b5498f5921ce90b0976e1b
Malik <malikhussein229@gmail.com>
2024-06-13 22:48:03 +0300

    Task#0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,10 @@
-# fix nginx to accept and serve more requests
+# Increase the limit of nginx requests
 
-exec {'modify max open files limit setting':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+exec { 'increase_limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+exec { 'restart_nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
